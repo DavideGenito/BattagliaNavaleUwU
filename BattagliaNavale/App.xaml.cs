ffcc559680da4f4db6e2d5c8bb419c6cbd39f180
Davide Genito <DAVIDE.GENITO@local.ittcesena.it>
2025-04-23 11:22:36 +0200

    uh uh

---
+++ BattagliaNavale/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BattagliaNavale
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BattagliaNavale/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BattagliaNavale
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
