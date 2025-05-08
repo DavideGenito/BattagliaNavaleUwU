09486f2151a8bcbe5eab2c393b26d853f24dbf52
Anna Tassinari <anna.tassinari.stud@ispascalcomandini.it>
2025-05-08 10:55:04 +0200

    home finita, mancano gli eventi

--- BattagliaNavale/Views/Home.xaml.cs
+++ BattagliaNavale/Views/Home.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Mvvm.Input;
+using System.Runtime.CompilerServices;
+
 namespace BattagliaNavale.Views;
 
 public partial class Home : ContentPage
@@ -5,5 +8,11 @@ public partial class Home : ContentPage
 	public Home()
 	{
 		InitializeComponent();
+	}
+
+	[RelayCommand]
+	public async Task OpenRegolamento()
+	{
+		await Navigation.PushAsync(new Regolamento());
 	}
 }
--- BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
+++ BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f55893747a80957bf51e56854ef206f9c7fb61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e7e60f68876fca369ca2aa7b56d59e911febd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
