e8f526fd77dbe896b0f2e5131d08fd70f461c321
DavideGenito <davidegenito2008@gmail.com>
2025-05-18 16:00:07 +0200

    boh

--- BattagliaNavale/obj/Debug/net8.0-ios/iossimulator-x64/BattagliaNavale.AssemblyInfo.cs
+++ BattagliaNavale/obj/Debug/net8.0-ios/iossimulator-x64/BattagliaNavale.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c2c27c215571e377099398134bda16c7fb52a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5caef8c89bc7a1d36401f610c7efb9d562270fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
