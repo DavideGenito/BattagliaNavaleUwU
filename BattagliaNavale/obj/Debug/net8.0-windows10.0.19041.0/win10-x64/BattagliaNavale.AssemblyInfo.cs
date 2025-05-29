b290d399dc171e25a560a629a6c755c273c71df4
Davide Genito <DAVIDE.GENITO@local.ittcesena.it>
2025-05-29 10:34:42 +0200

    faf

--- BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
+++ BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a191cfea27f7f18f4d75f485023ed916c6dd3bb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+10cc398ce5597a1532f5f31c5af7043c2c3b265a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
