e4007c7f601a2e71f1c71a4ccd99efb49f6207bd
Davide Genito <DAVIDE.GENITO@local.ittcesena.it>
2025-05-29 10:32:18 +0200

    screen

--- BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
+++ BattagliaNavale/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BattagliaNavale.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+10859a6aa9b24efabb8b80c50b0a893fddec433f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5caf126c55a6c938b3784d8b9153fcdc08ac0067")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattagliaNavale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
